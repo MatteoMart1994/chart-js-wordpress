f33a3fa4602cfc14605e417f1c1ae9e4d1d54474
MatteoMart1994 <54723298+MatteoMart1994@users.noreply.github.com>
2021-10-24 12:24:04 +0200

    Update trial_chart.js

--- trial_chart.js
+++ trial_chart.js
@@ -1,41 +1,75 @@
 $(document).ready(function () {
-	showDisticnt();
+    var queryDict = {}
+    window.location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
+    if (typeof queryDict['metrics'] !== "undefined" ){
+    let element_type = document.getElementById('type');
+    element_type.value = queryDict['metrics'];} 
+    if (typeof queryDict['season'] !== "undefined"){
+    let element_season = document.getElementById('league-season');
+    element_season.value = queryDict['season'];    
+}
+    if (typeof queryDict['cumulated'] !== "undefined"){
+    document.getElementById('checkbox1').checked = (queryDict['cumulated'] === 'true');
+}
+    if (typeof queryDict['date_start'] !== "undefined"){
+        window.selected_window = queryDict['date_start'];
+}
+    if (typeof queryDict['date_end'] !== "undefined"){
+        window.selected_window_end = queryDict['date_end'];
+}
+    showDisticnt();
+
 });
-$('#type').on('change', function(){
-        showDisticnt();
+
+function set_pars_url(key, value){
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set(key, value);
+    window.history.pushState( {} , "", "?"+urlParams.toString() );
+    }
+
+$('#league-season').on('change', function(){
+    var url = $(this).val();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('season', url);
+    window.location.search = urlParams;
+    // window.history.pushState( {} , "", "?"+urlParams.toString() );
+    showDisticnt(undefined,season_league=url);
 });
 
 $('#select_button').on('change', function(){
-		window.selected_window = this.value;
-        showDisticnt();
+    window.selected_window = $(this).val();
+    set_pars_url('date_start', window.selected_window);
+    showDisticnt();
 });
 
 $('#select_button_end').on('change', function(){
-        window.selected_window_end = this.value;
+        window.selected_window_end = $(this).val();
+        set_pars_url('date_end', window.selected_window_end);
 		showDisticnt();
 });
 
 $('#checkbox1').change(function() {
-    if(this.checked) {
-        window.integrated_series = true;
-    }
-    else{
-        window.integrated_series = false;
-    }
+    set_pars_url('cumulated', this.checked);
     showDisticnt();
 });
-
+$('#type').on('change', function(){
+    var metrics = $(this).val();
+    set_pars_url('metrics', metrics);
+    showDisticnt(col_evaluation=metrics);
+});
 
 $(window).bind("resize", function(){
     var userAgent = window.navigator.userAgent,
     platform = window.navigator.platform,
     macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
     windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+  //   iosPlatforms = ['iPhone', 'iPad', 'iPod'],
     os = null;
 
     if (macosPlatforms.indexOf(platform) !== -1) {
     os = 'Mac OS';
     } else if (
+    //   iosPlatforms.indexOf(platform) !== -1)
     /iPad|iPhone|iPod/.test(platform) ||
     (platform === 'MacIntel' && window.navigator.maxTouchPoints > 1))
     {
@@ -67,7 +101,7 @@ $(window).bind("resize", function(){
 });
 
 
-window.chartColors ={'Parma Calcio 1913': [
+ window.chartColors ={'Parma Calcio 1913': [
     "#ffd200",
     "#1b4094"],
 'AC Milan': [
@@ -77,7 +111,7 @@ window.chartColors ={'Parma Calcio 1913': [
 'Fiorentina': [
     "#482e92",
     "#482e92"
-], 
+],
 'Torino': [
     "#8a1e03",
     "#8a1e03"
@@ -137,7 +171,10 @@ window.chartColors ={'Parma Calcio 1913': [
     "#0068A8"
 ], 
 'Spezia': ['#7F7F7F', '#7F7F7F'],
-'Udinese': ["#000000", "#8B7D37"] 
+'Udinese': ["#000000", "#8B7D37"],
+'Salernitana': ["#8a1e03","#8a1e03"],
+'Venezia': ["#00A752", "#ff9900"],
+'Empoli': ["#00579C", "#00579C"]
 };
 
 // Create select drop_down
@@ -147,15 +184,21 @@ if (x.length <= 1){
 for (i=start; i<= length - 1;i++){
 var option = document.createElement("option");
 option.text= `${i}`;
-x.add(option)};}}
+x.add(option)};}
+}
 
-function showDisticnt(){
-    var col_evaluation = document.getElementById('type').value;
-  {
+function showDisticnt(col_evaluation=document.getElementById('type').value, season_league=document.getElementById('league-season').value){
+    // var col_evaluation = document.getElementById('type').value;
+    // var season_league = document.getElementById('league-season').value;
+    // document.getElementById('select_button').value = window.selected_window;
+    // document.getElementById('select_button_end').value = window.selected_window_end;
+
+    {
     $.post(my_ajax_object.ajax_url,
         {
             action: 'query_db',
-            selection: col_evaluation
+            selection: col_evaluation,
+            league: season_league
         },
     function (data)
     {
@@ -166,6 +209,7 @@ function showDisticnt(){
         labels = [];
         array_giornata = [];
         team_matches = [];
+        // console.log(json_data["AC Milan"]["h_a"]);
 
         Object.values(json_data).forEach(element => {
             team_matches.push(element['value'].length);
@@ -193,9 +237,11 @@ function showDisticnt(){
         for (var i=parseInt(window.considered_start); i<= parseInt(window.considered_end);i++){
 			array_giornata.push(`${i}`)};
 		// Create select drop_down
-		create_drop_down_window("select_button", 1, total_length);
+		create_drop_down_window("select_button", window.considered_start, total_length);
         
-        create_drop_down_window("select_button_end", 1, window.considered_end+1);
+        // create_drop_down_window("select_button_end", window.considered_start, window.considered_end+1);
+        create_drop_down_window("select_button_end", window.considered_start, parseInt(window.considered_end) +1);
+        // document.getElementById("select_button_end").value = window.selected_window_end;
 
         // Hide smaller values
         var op = document.getElementById("select_button_end").getElementsByTagName("option");
@@ -210,12 +256,15 @@ function showDisticnt(){
 
         opponent_dict = []
         h_a_dict = []
+        // window.Graph.config.options.legend.onClick https://www.chartjs.org/docs/3.4.0/configuration/interactions.html
+
         for (var team in json_data) {
             var colorName = window.chartColors[team][0];
-            if  (typeof window.integrated_series == 'undefined' || window.integrated_series == false){
+            var integrated_flg = document.getElementById('checkbox1').checked;
+            if  (typeof integrated_flg == 'undefined' || integrated_flg == false){
                 var data = json_data[team]["value"].slice(window.considered_start-1, window.considered_end);
             }
-            else if (window.integrated_series == true){
+            else if (integrated_flg == true){
                 var data = json_data[team]["value"].slice(window.considered_start-1, window.considered_end).map(function (val) { 
                     if (val == null){
                         val = 0;
@@ -259,6 +308,11 @@ function showDisticnt(){
                 ,
                 responsive: true,
                 maintainAspectRatio: false,
+				// title: {
+				// 	fontSize: 24,
+				// 	display: true,
+				// 	text: 'Understat Data'
+				// },
 				tooltips: {
 					mode: 'index',
 					intersect: false,
@@ -270,7 +324,7 @@ function showDisticnt(){
                 tooltips: {
                     callbacks: {
                         title: function(tooltipItems, data) {
-                           if (window.integrated_series && tooltipItems[0].index > 0){
+                           if (integrated_flg && tooltipItems[0].index > 0){
                             var value =  data.datasets[tooltipItems[0].datasetIndex].data[tooltipItems[0].index] - data.datasets[tooltipItems[0].datasetIndex].data[tooltipItems[0].index -1];
                         }
                         else {
@@ -297,6 +351,7 @@ function showDisticnt(){
                             }
 
                             return 'Opponent' + ': ' + opponent + ', ' + h_a_trad;
+                            // return ['Opponent' + ': ' + opponent + ', ' + h_a_trad, 'Home/Away' + ': ' + h_a.toUpperCase()];
                         },
                         label: function(tooltipItems, data) {
                             var label_team = data.datasets[tooltipItems.datasetIndex].label;  
@@ -326,6 +381,7 @@ function showDisticnt(){
         }
     
     if (window.Graph != null){
+        // console.log("il grafico esiste già, lo distruggo!");
         var list_hidden = window.Graph.legend.legendItems.map(function(arr) { return arr.hidden})
 
         for (var i in list_hidden) {
@@ -335,16 +391,31 @@ function showDisticnt(){
         
         }
     else{
-        // console.log("Graph creating...");
+        // console.log("Sto creando per la prima volta il grafico!");
     }
 
 
     var graphTarget = document.getElementById('graphCanvas').getContext('2d');
     window.Graph = new Chart(graphTarget, config);
 
+//  window.Graph.data.datasets[0]._meta[Object.keys(window.Graph.data.datasets[0]._meta)[0]].hidden
+// data.datasets[0]._meta[1].dataset.hidden
     $("#toggle").click(function() {
+        // console.log("Ho cliccato");
+        // console.log(window.Graph);
         window.Graph.data.datasets.forEach(function(ds) {
+        // if(ds._meta[1].hidden== null){
+            // if(ds._meta[Object.keys(ds._meta)[0]].hidden){
+            //     ds._meta[Object.keys(ds._meta)[0]].hidden = null;
+            // }
+            // else{
                 ds._meta[Object.keys(ds._meta)[0]].hidden = true;
+                // }
+            
+        // };
+        // ds._meta[1].hidden = true;
+        // ds._meta[Object.keys(ds._meta)[0]].hidden = true;
+            // console.log(ds._meta[1].hidden);
         });
         window.Graph.update();
     });
